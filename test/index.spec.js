cb25b430e50bd8ae4e8910a89382c6fd0970b769
Nathan Sculli <scull7@gmail.com>
2016-09-21 15:17:08 -0700

    add kvasir factory

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,112 +1,50 @@
 /*eslint-env node, mocha*/
-const { expect } = require('chai');
-const sinon      = require('sinon');
-const Kvasir     = require('../index');
+const demand      = require('must')
+const MemoryStore = require('kuss/lib/memory')
+const Kvasir      = require('../index')
 
 const TEST_NAME  = 'test';
-let testDomain   = null;
 
 describe('kvasir', function() {
 
-  beforeEach(() => testDomain = Kvasir(TEST_NAME));
+  let test_domain = null;
+  let test_driver = null;
 
-  it('should be a function with an arity of one', () => {
+  beforeEach(function() {
+    test_driver = MemoryStore({})
 
-    expect(Kvasir).to.be.a('function');
-    expect(Kvasir.length).to.eql(1);
-
-  });
-
-
-  it('should return a domain object', () => {
-
-    const domain = Kvasir('test');
-
-    expect(domain.find).to.be.a('function');
-    expect(domain.findOne).to.be.a('function');
-    expect(domain.findById).to.be.a('function');
-    expect(domain.save).to.be.a('function');
-
-  });
+    test_domain = Kvasir(TEST_NAME, (base, bucket) => ({
+        myCustomFind: (driver, value) => driver.findBy(bucket, 'foo', value)
+    }))
+  })
 
 
-  describe('::find', function() {
+  it('should have be a method with an arity of 2', function() {
 
-    it('should call the given driver with the name and terms', () => {
+    demand(Kvasir).be.a(Function)
+    demand(Kvasir.length).eql(2)
 
-      const driver = { find: sinon.stub().returns(42) };
-      const terms  = { foo: 'bar' };
-      const actual = testDomain.find(driver, terms);
-
-      expect(actual).to.eql(42);
-      expect(driver.find.calledOnce).to.be.true;
-      expect(driver.find.calledWith(TEST_NAME, terms)).to.be.true;
-    });
-
-  });
+  })
 
 
-  describe('::findOne', function() {
+  it('should be able to insert and retrieve data', function() {
 
-    it('should call the given driver with the name and terms', () => {
+    const test_data = { foo: 'bar', bar: 'baz', baz: 'buzz' }
 
-      const driver = { findOne: sinon.stub().returns(42) };
-      const terms  = { foo: 'bar' };
-      const actual = testDomain.findOne(driver, terms);
+    return test_domain.insert(test_driver, test_data)
 
-      expect(actual).to.eql(42);
-      expect(driver.findOne.calledOnce).to.be.true;
-      expect(driver.findOne.calledWith(TEST_NAME, terms)).to.be.true;
-    });
+    .then((id) => test_domain.getById(test_driver, id))
 
-  });
+    .then((record) => {
+      
+      demand(record.id).be.a.string()
+      demand(record.foo).eql('bar')
+      demand(record.bar).eql('baz')
+      demand(record.baz).eql('buzz')
 
+    })
 
-  describe('::findById', function() {
-
-    it('should call the given driver with the name and terms', () => {
-
-      const driver = { findById: sinon.stub().returns(42) };
-      const terms  = 42;
-      const actual = testDomain.findById(driver, terms);
-
-      expect(actual).to.eql(42);
-      expect(driver.findById.calledOnce).to.be.true;
-      expect(driver.findById.calledWith(TEST_NAME, terms)).to.be.true;
-    });
-
-  });
+  })
 
 
-  describe('::save', function() {
-
-    it('should call the insert method when current is null', () => {
-
-      const driver = { create: sinon.stub().returns(42) };
-      const terms  = { foo: 'bar' };
-      const actual = testDomain.save(driver, terms);
-
-      expect(actual).to.eql(42);
-      expect(driver.create.calledOnce).to.be.true;
-      expect(driver.create.calledWith(TEST_NAME, terms)).to.be.true;
-
-    });
-
-    it('should call the update method when current is not null', () => {
-
-      const driver  = { update: sinon.stub().returns(42) };
-      const request = { foo: 'bar' };
-      const current = { id: 123 };
-      const actual  = testDomain.save(driver, request, current);
-
-      expect(actual).to.eql(42);
-      expect(driver.update.calledOnce).to.be.true;
-      expect(driver.update.calledWith(TEST_NAME, request, current)).to.be.true;
-
-    });
-
-       
-
-  });
-
-});
+})
